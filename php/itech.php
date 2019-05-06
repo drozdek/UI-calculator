e026395825bab4ff60479ae2eafa9f4133d9a482
rafal <raf.drozdowski@gmail.com>
2019-05-06 09:43:24 +0100

    get date from ajax detect current ip address

--- php/itech.php
+++ php/itech.php
@@ -2,7 +2,34 @@
 /**
  * get uesrs data 
  */
-$data = $_POST['data'];
+$data = isset($_POST['data']) ? $_POST['data'] : '';
+
+/**
+ * create container obj to contain 
+ * properties passed via ajax call
+ * 
+ */
+$obj = new stdClass();
+$obj->calculationData = $data['data'];
+$obj->browser = $data['browser'];
+$obj->date = $data['date'];
+$obj->ip = $_SERVER['SERVER_ADDR'];
+
+/**
+ * @method getUserIpAddr get IP address
+ * @return {string} $ip
+ */
+function getUserIpAddr(){
+  if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+      //ip from shared internet
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
+  }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+      //ip pass from proxy
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }else{
+      $ip = $_SERVER['REMOTE_ADDR'];
+  }
+  return $ip;
 
 echo $data;
 ?>
